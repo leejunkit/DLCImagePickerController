843aac4ada2237b5bd198a7a3e004ffb39194c91
Lee Jun Kit <mohawk.riceball@gmail.com>
2012-12-04 17:36:17 +0800

    modified pan to use a UIScrollView instead of constantly re-rendering the radial blur

---
+++ Classes/BlurOverlay.h
@@ -0,0 +1,13 @@
+//
+//  BlurOverlay.h
+//  DLCImagePickerController
+//
+//  Created by Lee Jun Kit on 4/12/12.
+//  Copyright (c) 2012 Backspaces Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BlurOverlay : UIView
+
+@end
---
+++ Classes/BlurOverlay.m
@@ -0,0 +1,13 @@
+//
+//  BlurOverlay.m
+//  DLCImagePickerController
+//
+//  Created by Lee Jun Kit on 4/12/12.
+//  Copyright (c) 2012 Backspaces Inc. All rights reserved.
+//
+
+#import "BlurOverlay.h"
+
+@implementation BlurOverlay
+
+@end
